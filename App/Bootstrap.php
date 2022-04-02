c82cfc4f12e195d36864ad8ccf96145aa5a8e99f
Zurret <zurret@pm.me>
2022-04-02 23:22:40 +0200

    update

--- App/Bootstrap.php
+++ App/Bootstrap.php
@@ -2,14 +2,16 @@
 
 declare(strict_types=1);
 
+session_name('core_session');
 session_start();
+
 ini_set('date.timezone', 'Europe/Berlin');
 
-require_once __DIR__.'/../_vendor/autoload.php';
+require_once __DIR__ . '/../_vendor/autoload.php';
 
 $app = new Core\App\App();
 $app->run();
 
-require_once __DIR__.'/ErrorHandler.php';
+require_once __DIR__ . '/ErrorHandler.php';
 
 return $app;
